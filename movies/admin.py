ee44a348904d22636ff9d51a4a875db42f673e24
test8140 <test8140@ukr.net>
2020-02-10 17:32:55 +0200

    We are writing our actions for admin panel django.

--- movies/admin.py
+++ movies/admin.py
@@ -44,6 +44,7 @@ class MovieAdmin(admin.ModelAdmin):
     save_on_top = True
     save_as = True
     list_editable = ("draft",)
+    actions = ['publish', 'unpublish']
     form = MovieAdminForm
     #readonly_fields = ("get_image",)
 
@@ -72,9 +73,32 @@ class MovieAdmin(admin.ModelAdmin):
     #def get_image(self, obj):
         #return = mark_safe(f'<img src={obj.poster.url} width="100" height="110"')
         
+
+    def unpublish(self, request, queryset):
+        row_update = queryset.update(draft=True)
+        if row_update == 1:
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f"{row_update} записей были обновлены"
+        self.message_user(request, f"{message_bit}")
+
+
+    def publish(self, request, queryset):
+        row_update = queryset.update(draft=False)
+        if row_update == 1:
+            message_bit = "1 запись была обновлена"
+        else:
+            message_bit = f"{row_update} записей были обновлены"
+        self.message_user(request, f"{message_bit}")
+
+    
+    publish.short_description = "Опубликовать"
+    publish.allowed_permission = ('change', )
+
+    unpublish.short_description = "Снять с публикации"
+    unpublish.allowed_permission = ('change', )
+
     #get_image.short_description = "Постер"
-
-
 
 
 @admin.register(Reviews)
